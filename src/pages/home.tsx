aff3c5fc2b4fb835c23c51b3ec39d4fa80208b02
Marvel Collin <marvelcollin7@gmail.com>
2024-12-06 13:09:42 +0700

    feat: add tetris background

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import LayeredTransition from '../components/page-animations/layered';
+import TetrisBackground from '../components/background-animations/tetris-background';
 import '../index.css';
 
 function Home() {
@@ -13,9 +14,10 @@ function Home() {
 
     return (
         <div className="bg-black w-full h-screen flex items-center justify-center font-game" onClick={handleClick}>
+            <TetrisBackground />
             {showTransition && !showWelcome && (
                 <LayeredTransition colors={['#000000', '#111111', '#222222', '#333333', '#444444', '#555555', '#666666']} onTransitionEnd={() => setShowWelcome(true)}>
-                    <div className="text-center transition-transform duration-1000 transform hover:scale-105 font-game flex flex-col items-center justify-center h-full">
+                    <div className="text-center transition-transform duration-1000 transform hover:scale-105 font-game flex flex-col items-center justify-center h-full opacity-100 transition-opacity duration-1000">
                         <h1 className="text-white animate-bounce text-7xl">NAR</h1>
                         <h2 className="text-white animate-pulse mt-2" style={{ fontSize: '10rem' }}>25-2</h2>
                         <p className="text-white mt-4">Click anywhere to continue</p>
@@ -24,8 +26,9 @@ function Home() {
             )}
             {showWelcome && (
                 <div className="relative bg-[#666666] z-10 opacity-100 transition-opacity duration-1000 flex items-center justify-center w-full h-full">
-                    <div className="text-center transition-transform duration-1000 transform hover:scale-105 font-game opacity-100 transition-opacity duration-1000">
+                    <div className="text-center transition-transform duration-1000 transform hover:scale-105 font-game">
                         <h1 className="text-white text-7xl">Welcome</h1>
+                        <p className="text-white mt-4">You are now on the welcome page</p>
                     </div>
                 </div>
             )}

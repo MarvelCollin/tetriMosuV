833da8ae58fbfcd6fc5c005fbcce412ac4d14a25
kolin <marvelcollin7@gmail.com>
2025-05-10 16:16:19 +0700

    feat: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tetriMosuV/', // Set base path to the repository name
 })
